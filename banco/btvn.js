fb1edfadd6853bda19a8a5552322c06d64706939
trong98dk <67058239+trong98dk@users.noreply.github.com>
2020-07-02 14:39:15 +0700

    BanCoVua

---
+++ banco/btvn.js
@@ -0,0 +1,88 @@
+var canvas = document.querySelector("#canvas")
+canvas.width = 1000
+canvas.height = 1000
+canvas.style.border = '2px solid brown'
+
+
+var ctx = canvas.getContext('2d')
+/* hàng 1*/
+ctx.beginPath()
+ctx.fillStyle = "white"
+ctx.fillRect(0, 0, 125, 125)
+ctx.fillStyle = "black"
+ctx.fillRect(125, 0, 125, 125)
+ctx.fillStyle = "white"
+ctx.fillRect(250, 0, 125, 125)
+ctx.fillStyle = "black"
+ctx.fillRect(375, 0, 125, 125)
+ctx.fillStyle = "white"
+ctx.fillRect(500, 0, 125, 125)
+ctx.fillStyle = "black"
+ctx.fillRect(625, 0, 125, 125)
+ctx.fillStyle = "white"
+ctx.fillRect(750, 0, 125, 125)
+ctx.fillStyle = "black"
+ctx.fillRect(875, 0, 125, 125)
+/* hàng 2 */
+ctx.fillStyle = "black"
+ctx.fillRect(0, 125, 125, 125)
+ctx.fillStyle = "black"
+ctx.fillRect(250, 125, 125, 125)
+ctx.fillStyle = "black"
+ctx.fillRect(500, 125, 125, 125)
+ctx.fillStyle = "black"
+ctx.fillRect(750, 125, 125, 125)
+/* hàng 3*/
+ctx.fillStyle = "black"
+ctx.fillRect(125, 250, 125, 125)
+ctx.fillStyle = "black"
+ctx.fillRect(375, 250, 125, 125)
+ctx.fillStyle = "black"
+ctx.fillRect(625, 250, 125, 125)
+ctx.fillStyle = "black"
+ctx.fillRect(875, 250, 125, 125)
+/* hàng 4*/
+ctx.fillStyle = "black"
+ctx.fillRect(0, 375, 125, 125)
+ctx.fillStyle = "black"
+ctx.fillRect(250, 375, 125, 125)
+ctx.fillStyle = "black"
+ctx.fillRect(500, 375, 125, 125)
+ctx.fillStyle = "black"
+ctx.fillRect(750, 375, 125, 125)
+/* hàng 5*/
+ctx.fillStyle = "black"
+ctx.fillRect(125, 500, 125, 125)
+ctx.fillStyle = "black"
+ctx.fillRect(375, 500, 125, 125)
+ctx.fillStyle = "black"
+ctx.fillRect(625, 500, 125, 125)
+ctx.fillStyle = "black"
+ctx.fillRect(875, 500, 125, 125)
+/* hàng 6*/
+ctx.fillStyle = "black"
+ctx.fillRect(0, 625, 125, 125)
+ctx.fillStyle = "black"
+ctx.fillRect(250, 625, 125, 125)
+ctx.fillStyle = "black"
+ctx.fillRect(500, 625, 125, 125)
+ctx.fillStyle = "black"
+ctx.fillRect(750, 625, 125, 125)
+/* hàng 7*/
+ctx.fillStyle = "black"
+ctx.fillRect(125, 750, 125, 125)
+ctx.fillStyle = "black"
+ctx.fillRect(375, 750, 125, 125)
+ctx.fillStyle = "black"
+ctx.fillRect(625, 750, 125, 125)
+ctx.fillStyle = "black"
+ctx.fillRect(875, 750, 125, 125)
+/* hàng 8*/
+ctx.fillStyle = "black"
+ctx.fillRect(0, 875, 125, 125)
+ctx.fillStyle = "black"
+ctx.fillRect(250, 875, 125, 125)
+ctx.fillStyle = "black"
+ctx.fillRect(500, 875, 125, 125)
+ctx.fillStyle = "black"
+ctx.fillRect(750, 875, 125, 125)
